854f3b7d45c2b9f743183d3db7e93971596858d9
Blaise Kal <blaisekal@gmail.com>
2021-05-17 21:05:31 +0200

    Add option to ask for confirmation

--- app/_locales/zh/messages.ts
+++ app/_locales/zh/messages.ts
@@ -1,7 +1,10 @@
 // tslint:disable max-line-length
-module.exports = JSON.stringify({
+module.exports = JSON.stringify(
+  {
     extensionName: { message: `忘了这个网站` },
-    extensionDescription: { message: `页面操作按钮,用于删除您正在访问的网站的cookie,本地存储,浏览历史记录和下载历史记录。` },
+    extensionDescription: {
+      message: `页面操作按钮,用于删除您正在访问的网站的cookie,本地存储,浏览历史记录和下载历史记录。`,
+    },
     successNotificationText: { message: `已成功删除数据` },
     successNotificationBody: { message: `$1 $2 删除$` },
     deleteItems: { message: `单击页面操作按钮时,<br>删除当前网站的以下项目:` },
@@ -10,10 +13,15 @@ module.exports = JSON.stringify({
     history: { message: `浏览历史记录` },
     downloads: { message: `下载历史记录` },
     beforeDeleting: { message: `在删除项目之前:` },
+    askConfirmation: { message: `要求确认` },
+    confirmationPrompt: { message: `您确定要删除$1吗？` },
     closeTab: { message: `关闭标签` },
     closeTabHelp: { message: `这可以防止网站在删除后写出其他项目` },
     afterDeleting: { message: `删除项目后:` },
     showConfirmation: { message: `显示确认` },
     reloadWebsite: { message: `重新加载网站` },
     lastSaved: { message: `上次保存: $1` },
-}, null, 2);
+  },
+  null,
+  2
+);
--- app/background.ts
+++ app/background.ts
@@ -5,93 +5,109 @@ import defaultOptions from "./options/defaults";
 const deleteOptions = ["cookies", "localStorage", "history", "downloads"];
 
 if (process.env.BROWSER !== "firefox") {
-    window.browser = require("webextension-polyfill");  // tslint:disable-line
+  window.browser = require("webextension-polyfill"); // tslint:disable-line
 }
 
 browser.tabs.onUpdated.addListener((tabId) => browser.pageAction.show(tabId));
 console.info("Forget about this site loaded");
 
 browser.pageAction.onClicked.addListener(async (tab) => {
+  const result = await browser.storage.sync.get("options");
+  const options = { ...defaultOptions, ...(result.options || {}) };
+  console.info({ options });
 
-    const result = await browser.storage.sync.get("options");
-    const options = {...defaultOptions, ...(result.options || {})};
-    console.info({options});
+  const taburl = new URL(tab.url);
+  const hostname = taburl.hostname;
+  const hostnameDomain = tldts.parse(hostname).domain;
+  const hostnames = Array.from(new Set([hostnameDomain, hostname]));
+  const origins = hostnames.map((h) => `${taburl.protocol}//${h}`);
+  const browsingDataQuery =
+    process.env.BROWSER === "firefox" ? { hostnames } : { origins };
+  console.info({ browsingDataQuery });
 
-    const taburl = new URL(tab.url);
-    const hostname = taburl.hostname;
-    const hostnameDomain = tldts.parse(hostname).domain;
-    const hostnames = Array.from(new Set([hostnameDomain, hostname]));
-    const origins = hostnames.map((h) => `${taburl.protocol}//${h}`);
-    const browsingDataQuery = (process.env.BROWSER === "firefox") ? {hostnames} : {origins};
-    console.info({browsingDataQuery});
+  const removeText = Object.keys(defaultOptions)
+    .filter((key) => deleteOptions.includes(key))
+    .map((key) => browser.i18n.getMessage(key).toLocaleLowerCase())
+    .join(", ");
 
-    const removeText = Object.keys(defaultOptions).filter(
-        (key) => deleteOptions.includes(key)
-    ).map((key) => browser.i18n.getMessage(key).toLocaleLowerCase()).join(", ");
+  if (options.askConfirmation) {
+    const ask = browser.i18n.getMessage("confirmationPrompt", [removeText]);
+    const confirm = await browser.tabs.executeScript({
+      // NotificationOptions.buttons not supported in Firefox.
+      code: `confirm(${JSON.stringify(ask)})`,
+    });
+    if (confirm && !confirm[0]) {
+      return;
+    }
+  }
 
-    const promises = [];
+  const promises = [];
 
+  if (options.closeTab) {
+    await browser.tabs.remove(tab.id);
+  }
+
+  if (options.history) {
+    promises.push(...(await removeHistory(hostnameDomain)));
+  }
+
+  if (options.downloads) {
+    promises.push(browser.downloads.erase({ query: hostnames }));
+  }
+
+  if (options.cookies) {
+    promises.push(browser.browsingData.removeCookies(browsingDataQuery));
+  }
+
+  if (options.localStorage) {
+    promises.push(browser.browsingData.removeLocalStorage(browsingDataQuery));
+  }
+
+  try {
+    console.info({ promises });
+    await Promise.all(promises);
+    console.info("All promises fulfilled, data deleted.");
+  } catch (error) {
+    console.warn({ error });
+    await browser.notifications.create({
+      type: "basic",
+      iconUrl: iconImage,
+      title: `Error removing data for ${hostnames[0]}`,
+      message: error,
+    });
+  }
+
+  if (options.showNotification) {
+    await browser.notifications.create({
+      type: "basic",
+      iconUrl: iconImage,
+      title: browser.i18n.getMessage("successNotificationText"),
+      message: browser.i18n.getMessage("successNotificationBody", [
+        removeText,
+        hostnameDomain,
+      ]),
+    });
+  }
+
+  if (options.refreshPage) {
     if (options.closeTab) {
-        await browser.tabs.remove(tab.id);
+      await browser.tabs.create({ url: tab.url });
+    } else {
+      await browser.tabs.reload(tab.id);
     }
-
-    if (options.history) {
-        promises.push(...await removeHistory(hostnameDomain));
-    }
-
-    if (options.downloads) {
-        promises.push(browser.downloads.erase({query: hostnames}));
-    }
-
-    if (options.cookies) {
-        promises.push(browser.browsingData.removeCookies(browsingDataQuery));
-    }
-
-    if (options.localStorage) {
-        promises.push(browser.browsingData.removeLocalStorage(browsingDataQuery));
-    }
-
-    try {
-        console.info({promises});
-        await Promise.all(promises);
-        console.info("All promises fulfilled, data deleted.");
-    } catch (error) {
-        console.warn({error});
-        await browser.notifications.create({
-            type: "basic",
-            iconUrl: iconImage,
-            title: `Error removing data for ${hostnames[0]}`,
-            message: error,
-        });
-    }
-
-    if (options.showNotification) {
-        await browser.notifications.create({
-            type: "basic",
-            iconUrl: iconImage,
-            title: browser.i18n.getMessage("successNotificationText"),
-            message: browser.i18n.getMessage("successNotificationBody", [removeText, hostnameDomain]),
-        });
-    }
-
-    if (options.refreshPage) {
-        if (options.closeTab) {
-            await browser.tabs.create({url: tab.url});
-        } else {
-            await browser.tabs.reload(tab.id);
-        }
-    }
-
+  }
 });
 
 async function removeHistory(hostnameDomain: string) {
-    const historyItems = await browser.history.search({
-        text: hostnameDomain,
-        startTime: 0,
-        maxResults: 1000,
-    });
-    console.info({historyItems});
-    return Promise.all(historyItems.map(
-        (historyItem) => browser.history.deleteUrl({url: historyItem.url})
-    ));
+  const historyItems = await browser.history.search({
+    text: hostnameDomain,
+    startTime: 0,
+    maxResults: 1000,
+  });
+  console.info({ historyItems });
+  return Promise.all(
+    historyItems.map((historyItem) =>
+      browser.history.deleteUrl({ url: historyItem.url })
+    )
+  );
 }
